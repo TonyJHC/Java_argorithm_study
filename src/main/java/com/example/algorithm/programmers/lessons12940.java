eea1578f5e9834b8cb6074649135070713faa034
TonyJHC <jongvin01@naver.com>
2021-10-01 23:17:41 +0900

    .

--- src/main/java/com/example/algorithm/programmers/lessons12940.java
+++ src/main/java/com/example/algorithm/programmers/lessons12940.java
@@ -3,6 +3,7 @@ package com.example.algorithm.programmers;
 
 public class lessons12940 {
     public String solution(String phone_number) {
+
         String answer = "";
 
         for (int i = 0; i < phone_number.length() - 4; i++) { // 맨 뒷자리 4자리 남겨두고
@@ -15,10 +16,12 @@ public class lessons12940 {
     }
 
     public static void main(String[] args) {
+
         lessons12940 lessons12940_2 = new lessons12940();
         String answer = lessons12940_2.solution("01094460770");
 
         System.out.println("answer :  " + answer);
+
     }
 
 }
