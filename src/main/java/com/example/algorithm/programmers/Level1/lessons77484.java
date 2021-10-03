cefe9aa0c142f954947a31958c2b98c3280265b2
TonyJHC <jongvin01@naver.com>
2021-10-03 17:50:41 +0900

    Arrays.stream() 공부 이어서 하기

--- src/main/java/com/example/algorithm/programmers/Level1/lessons77484.java
+++ src/main/java/com/example/algorithm/programmers/Level1/lessons77484.java
@@ -28,8 +28,8 @@ public class lessons77484 {
         }
 
         // 2. 0을 제외한 당첨번호 비교 --> 갯수 찾기
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
+        for (int i = 0; i < lottos.length; i++) {
+            for (int j = 0; j < lottos.length; j++) {
                 if (lottos[i] == win_nums[j])
                     correctCnt++;
             }
@@ -49,8 +49,8 @@ public class lessons77484 {
         //int low = correctCnt;
 
         // 3. 결과값 정하기
-        answer[0] = 6 - (correctCnt + zeroCnt) + 1; // 최고 등수
-        answer[1] = 6 - correctCnt + 1; // 최저 등수
+        answer[0] = lottos.length - (correctCnt + zeroCnt) + 1; // 최고 등수
+        answer[1] = lottos.length - correctCnt + 1; // 최저 등수
 
         return answer;
     }
@@ -68,3 +68,21 @@ public class lessons77484 {
     }
 
 }
+
+
+/*
+참고 : 자바 공홈 https://dev.java/learn/tutorial/mastering-the-api/stream-api/creating-streams.html
+<스트림>
+: '데이터의 흐름'
+장점 : 배열 또는 인스턴스에 함수 여러 개를 조합하여 원하는 결과를 필터링하고 가공된 결과를 얻을 수 있음.
+    또한 람다를 이용하여 코드의 양을 줄이고 간결하게 표현할 수 있음. ( 배열과 인스턴스를 함수형으로 처리가능 )
+    간단하게 병렬처리 ( multi-threading ) 가능. : 하나의 작업을 둘 이상의 작업으로 잘게 나눠서 동시에 진행 -> 쓰레드를 이용하여 많은 요소들을 빠르게 처리가능.
+
+* public int[] solution(int[] lottos, int[] winNums) {
+        return LongStream.of(
+                (lottos.length + 1) - Arrays.stream(lottos).filter(l -> Arrays.stream(winNums).anyMatch(w -> w == l) || l == 0).count(),
+                (lottos.length + 1) - Arrays.stream(lottos).filter(l -> Arrays.stream(winNums).anyMatch(w -> w == l)).count()
+        )
+                .mapToInt(op -> (int) (op > 6 ? op - 1 : op))
+                .toArray();
+    }*/
