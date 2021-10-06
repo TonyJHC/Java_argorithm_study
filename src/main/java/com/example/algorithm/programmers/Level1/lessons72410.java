8c5028391c92ea6a2c224317893f3791f9c4ec69
TonyJHC <jongvin01@naver.com>
2021-10-07 00:31:34 +0900

    kakao ID

---
+++ src/main/java/com/example/algorithm/programmers/Level1/lessons72410.java
@@ -0,0 +1,17 @@
+package com.example.algorithm.programmers.Level1;
+
+public class lessons72410 {
+    public String solution(String new_id) {
+        String answer = "";
+        String lower = new
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        lessons72410 lessons72410 = new lessons72410();
+
+        String result = lessons72410.solution("...!@BaT#*..y.abcdefghijklm");
+
+
+    }
+}
