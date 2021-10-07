99df9482008e47489a363d1aca1a9f68da7e6890
TonyJHC <jongvin01@naver.com>
2021-10-07 18:54:00 +0900

    4단계까지 완료

--- src/main/java/com/example/algorithm/programmers/Level1/lessons72410.java
+++ src/main/java/com/example/algorithm/programmers/Level1/lessons72410.java
@@ -7,14 +7,43 @@ import java.util.Arrays;
 public class lessons72410 {
     public String solution(String new_id) {
         String answer = "";
+
+        // 1단계
         new_id = new_id.toLowerCase();
 
-        String newId = "";
+        String newId2 = "";
 
+        // 2단계
         for (int i = 0; i < new_id.length(); i++) {
             char tmp = new_id.charAt(i);
-            if (tmp == '')
+            if (tmp >= 'a' && tmp <= 'z') // 소문자 일때
+                newId2 += tmp; // 추가
+            else if (tmp == '-')
+                newId2 += tmp;
+            else if (tmp == '_')
+                newId2 += tmp;
+            else if (tmp == '.')
+                newId2 += tmp;
         }
+
+        String newId3 = "";
+        // 3단계 :  마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환
+        for (int i = 0; i < newId2.length()-1; i++) {
+//            char tmp1 = newId2.charAt(i);
+//            char tmp2 = newId2.charAt(i+1);
+
+            if (newId2.charAt(i) != '.' || newId2.charAt(i + 1) != '.')
+                newId3 += newId2.charAt(i);
+        }
+
+//        String newId4 = "";
+        // 4단계 : 4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
+        if(newId3.charAt(0) == '.')
+            newId3 = newId3.substring(1);
+        else if(newId3.charAt(newId3.length()) == '.')
+            newId3 = newId3.substring(0,newId3.length()-1);
+
+        answer =newId3;
 
 
 /*
@@ -84,7 +113,7 @@ public class lessons72410 {
         lessons72410 lessons72410 = new lessons72410();
         System.out.println("...!@BaT#*..y.abcdefghijklm");
 
-        String result = lessons72410.solution("z-+.^.");
+        String result = lessons72410.solution("...!@BaT#*..y.abcdefghijklm");
 
         // String result = lessons72410.solution("123_.def");
 //        String str1 = "123123asdf";
