229654e3d930b648fc5a2bfbfcf368eb71ffdb7c
TonyJHC <jongvin01@naver.com>
2021-10-22 02:21:28 +0900

    거리두기 확인하기

--- src/main/java/com/example/algorithm/programmers/Level1/test.java
+++ src/main/java/com/example/algorithm/programmers/Level1/test.java
@@ -3,11 +3,10 @@ package com.example.algorithm.programmers.Level1;
 public class test {
 
     public static void main(String[] args) {
-        String str1 = "abcabcabcabcdededededede";
-        System.out.println(str1.length());
-        System.out.println(str1.substring(0,3));
-        System.out.println(str1);
-        System.out.println(str1.substring(0,str1.length()));
-
+        String test ="POOPX";
+        int cnt =0;
+        while(cnt++ <5) {
+            System.out.println(test.indexOf("P"));
+        }
     }
 }
