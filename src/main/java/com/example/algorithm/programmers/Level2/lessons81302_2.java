7859a315ec70c4d455240bb1c4754e75498da3af
TonyJHC <jongvin01@naver.com>
2021-10-29 16:53:15 +0900

    인덱스변경

--- src/main/java/com/example/algorithm/programmers/Level2/lessons81302_2.java
+++ src/main/java/com/example/algorithm/programmers/Level2/lessons81302_2.java
@@ -32,20 +32,20 @@ public class lessons81302_2 {
 //
         for (int p = 0; p < 5; p++) { // 대기실
             for (int c = 0; c < 5; c++) { //  행
-                for (int r = 0; r < 5; r++) {
+                for (int r = 0; r < 5; r++) { // 열
                     char cpr = strc[p][r][c];
                     // 1. 같은 행에서 거리 검사
                     for (int i = r + 1; i < 5; i++) {
                         if ((Math.abs(c - c) + Math.abs(r - i)) == 1) {// 거리가 1일때
-                            if ((strc[p][r][c] == 'P' && strc[p][r][i] == 'P')) {  // 둘다 자리에 누가 있다면
-                                System.out.println(p + ", " + r + ", " + c +"거리 1 : 같은 column에 위치한 좌석 " + strc[p][r][c] + " , " + strc[p][r][i]);
+                            if ((strc[p][r][c] == 'P' && strc[p][i][c] == 'P')) {  // 둘다 자리에 누가 있다면
+                                System.out.println(p + "번 대기실 (" + c + ", " + r +")"+" (" + i + ", " + c +")"+" / 거리 1 : 같은 row에 위치한 좌석 " + strc[p][r][c] + " , " + strc[p][i][c]);
                                 answer[p] = 0; // 해당 대기실 방역수칙 어김 : 0
 
                             }
                         } else if ((Math.abs(c - c) + Math.abs(r - i)) == 2) { // 거리가 2일 때
-                            if ((strc[p][r][c] == 'P' && strc[p][r][i] == 'P')) {
-                                if ((r + 1) < 5 && strc[p][r][r + 1] != 'X') { // 사이에 파티션이 없다면 ** 주의 : 오버플로우 , 에러안나면 무시
-                                    System.out.println(p + ", " + r + ", " + c +"거리 2 : 같은 column에 위치한 좌석 " + strc[p][r][c] + " , " + strc[p][r][c + 1]);
+                            if ((strc[p][r][c] == 'P' && strc[p][i][c] == 'P')) {
+                                if ((i + 1) < 5 && strc[p][i + 1][c] != 'X') { // 사이에 파티션이 없다면 ** 주의 : 오버플로우 , 에러안나면 무시
+                                    System.out.println(p + "번 대기실 (" + c + ", " + r +")"+" (" + i + ", " + c +")"+" / 거리 2 : 같은 row에 위치한 좌석 " + strc[p][r][c] + " , " + strc[p][i][c]);
                                     answer[p] = 0; // 해당 대기실 방역수칙 어김 : 0
                                 }
                             }
@@ -55,32 +55,45 @@ public class lessons81302_2 {
                     // 2. 같은 열에서 거리 검사
                     for (int i = c + 1; i < 5; i++) {
                         if (Math.abs(c - i) + Math.abs(r - r) == 1) {// 거리가 1일때
-                            if ((strc[p][r][c] == 'P' && strc[p][i][c] == 'P')) {  // 둘다 자리에 누가 있다면
-                                System.out.println(p + ", " + r + ", " + c +"거리 1 : 같은 column에 위치한 좌석 " + strc[p][r][c] + " , " + strc[p][i][c]);
+                            if ((strc[p][r][c] == 'P' && strc[p][r][i] == 'P')) {  // 둘다 자리에 누가 있다면
+                                System.out.println(p + "번 대기실 (" + c + ", " + r +")"+"번 대기실 (" + r + ", " + i +")"+" / 거리 1 : 같은 row에 위치한 좌석 " + strc[p][r][c] + " , " + strc[p][r][i]);
 
                                 answer[p] = 0; // 해당 대기실 방역수칙 어김 : 0
 
                             }
                         } else if (Math.abs(c - i) + Math.abs(r - r) == 2) { // 거리가 2일 때
-                            if ((strc[p][r][c] == 'P' && strc[p][i][c] == 'P')) {
-                                if ((r + 1) < 5 && strc[p][r + 1][c] != 'X') // 사이에 파티션이 없다면 ** 주의 : 오버플로우 , 에러안나면 무시
-                                    System.out.println(p + ", " + r + ", " + c +"거리 2 : 같은 column에 위치한 좌석 " + strc[p][r][c] + " , " + strc[p][i][c]);
+                            if ((strc[p][r][c] == 'P' && strc[p][r][i] == 'P')) {
+                                if ((i + 1) < 5 && strc[p][r][i+1] != 'X') // 사이에 파티션이 없다면 ** 주의 : 오버플로우 , 에러안나면 무시
+                                    System.out.println(p + "번 대기실 (" + c + ", " + r +")"+"번 대기실 (" + r + ", " + i +")"+" / 거리 2 : 같은 column에 위치한 좌석 " + strc[p][r][c] + " , " + strc[p][r][i]);
                                 answer[p] = 0; // 해당 대기실 방역수칙 어김 : 0
                             }
                         }
                     }
 
-                    // 3. 대각선 조사
+                    // 3. 대각선 조사 - 우측
                     for (int i = 1; i < 5 - r; i++) {
                         if (Math.abs(c - (c + i)) + Math.abs(r - (r + i)) == 2) {// 거리가 2일때
                             if ((c + i) < 5 && (r + i) < 5 && (strc[p][r][c] == 'P' && strc[p][r + i][c + i] == 'P')) {  // 둘다 자리에 누가 있다면
-//                                if (strc[p][r + 1][c] != 'X' && strc[p][r][c + 1] != 'X') {
-                                System.out.println(p + ", " + r + ", " + c + " 거리 2 : 같은 column에 위치한 좌석 " + strc[p][r][c] + " , " + strc[p][r + i][c + i]);
+                               if (strc[p][r + 1][c] != 'X' && strc[p][r][c + 1] != 'X') {
+                                System.out.println("대각선  "+ p + ", " + r + ", " + c + " 거리 2 : 같은 우측 대각선에 위치한 좌석 " + strc[p][r][c] + " , " + strc[p][r + i][c + i]);
                                 answer[p] = 0; // 해당 대기실 방역수칙 어김 : 0
-//                                }
+                               }
                             }
                         }
-                    }//
+                    }
+                    // 3. 대각선 조사 - 좌측
+                    for (int i = 1; i <5-r; i++) {
+                        if (Math.abs(c - (c - i)) + Math.abs(r - (r - i)) == 2) {// 거리가 2일때
+                            if ((c - i) > -1 && (r - i) > -1 && (strc[p][r][c] == 'P' && strc[p][r - i][c - i] == 'P')) {  // 둘다 자리에 누가 있다면
+                                if (strc[p][r - 1][c] != 'X' && strc[p][r][c - 1] != 'X') {
+                                    System.out.println("대각선  "+p + ", " + r + ", " + c + " 거리 2 : 같은 좌측 대각선에 위치한 좌석 " + strc[p][r][c] + " , " + strc[p][r + i][c + i]);
+                                    answer[p] = 0; // 해당 대기실 방역수칙 어김 : 0
+                                }
+                            }
+                        }
+                    }
+
+
                 }
             }
         }
